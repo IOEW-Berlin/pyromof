004da6f598215979884214c02a0709b9da330a45
Max4375 <klimabe@web.de>
2026-08-01 14:26:39 +0200

    Adjust opex policy postprocessing to second electricity sink

--- pyromof/postprocessing_functions/opex_policies.py
+++ pyromof/postprocessing_functions/opex_policies.py
@@ -10,36 +10,42 @@ from pyromof.preprocessing_functions.implement_policies import (
 )
 
 
-def receive_data(data: dict) -> tuple[pd.Series, pd.Series, float, float]:
+def receive_data(data: dict) -> tuple[pd.Series, pd.Series, pd.Series]:
     scenario = retrieve_scenario_from_input_data(data["general"])
 
-    pyrolysis_electricity_output = pd.read_csv(
+    sequences = pd.read_csv(
         f"./results/{scenario}/results/sequences.csv", sep=";", index_col=0, parse_dates=True
-    )["b_electricity to electricity_grid"]
+    )
+    subsidized_electricity_output = sequences["b_electricity to electricity_grid_subsidy"]
+    unsubsidized_electricity_output = sequences["b_electricity to electricity_grid"]
 
     electricity_price = receive_and_refine_electricity_price_data(data["profiles"])
 
-    return (electricity_price, pyrolysis_electricity_output)
+    return (electricity_price, subsidized_electricity_output, unsubsidized_electricity_output)
 
 
 def postprocess_feed_in_tariff(data):
 
-    electricity_price, pyrolysis_electricity_output = receive_data(data)
+    electricity_price, subsidized_electricity_output, unsubsidized_electricity_output = (
+        receive_data(data)
+    )
 
-    feed_in_tarrif = (
+    feed_in_tariff = (
         -1
         / 100
         * data["policies"].loc[data["policies"]["policy"] == "feed in tariff", "value 1"].values[0]
     )
-    government_payment_share = feed_in_tarrif - electricity_price
+    government_payment_share = feed_in_tariff - electricity_price
 
-    feed_in_revenue = feed_in_tarrif * pyrolysis_electricity_output
-    governemt_payment = government_payment_share * pyrolysis_electricity_output
-    market_payment = electricity_price * pyrolysis_electricity_output
+    total_electricity_output = subsidized_electricity_output + unsubsidized_electricity_output
+    market_payment = electricity_price * total_electricity_output
+    government_payment = government_payment_share * subsidized_electricity_output
+    feed_in_revenue = market_payment + government_payment
 
-    sum_feed_in_revenue = feed_in_revenue.sum().round(1)
-    sum_government_payment = governemt_payment.sum().round(1)
     sum_market_payment = market_payment.sum().round(1)
+    sum_government_payment = government_payment.sum().round(1)
+    sum_feed_in_revenue = feed_in_revenue.sum().round(1)
+
     return {
         "feed_in_revenue": sum_feed_in_revenue,
         "government_payment": sum_government_payment,
@@ -49,15 +55,15 @@ def postprocess_feed_in_tariff(data):
 
 def postprocess_sliding_premium(data):
     # receive input data
-    electricity_price, pyrolysis_electricity_output = receive_data(data)
-    feed_in_revenue_euro_per_kwh, sliding_premium, _ = feed_in_payment_sliding_premium(data)
-
+    electricity_price, subsidized_electricity_output, unsubsidized_electricity_output = (
+        receive_data(data)
+    )
+    _, sliding_premium, _ = feed_in_payment_sliding_premium(data)
+    total_electricity_output = subsidized_electricity_output + unsubsidized_electricity_output
     # calculate payments
-    feed_in_revenue = feed_in_revenue_euro_per_kwh * pyrolysis_electricity_output
-
-    government_payment = sliding_premium * pyrolysis_electricity_output
-
-    market_payment = electricity_price * pyrolysis_electricity_output
+    market_payment = electricity_price * total_electricity_output
+    government_payment = sliding_premium * subsidized_electricity_output
+    feed_in_revenue = market_payment + government_payment
 
     # calculate payment sums
     sum_government_payment = government_payment.sum().round(1)
