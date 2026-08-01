4c94691babcafc73221f682bc725386103f98e61
Max4375 <klimabe@web.de>
2026-08-01 14:24:42 +0200

    Implement evaluation of flex bonus policy

--- pyromof/postprocessing_functions/capex_policies.py
+++ pyromof/postprocessing_functions/capex_policies.py
@@ -20,6 +20,39 @@ def receive_investment(data, variable_name, type_name):
     return float(result)
 
 
+def receive_chp_capacity(data, scalar_objective):
+    investment = (
+        data["converters"].loc[data["converters"]["label"] == "chp", "investment"].values[0]
+    )
+
+    if not investment:
+        chp_capacity = float(
+            data["converters"]
+            .loc[data["converters"]["label"] == "chp", "nominal_capacity"]
+            .values[0]
+        )
+    else:
+        chp_capacity = receive_investment(data, scalar_objective, "built capacity [kW]")
+    return chp_capacity
+
+
+def flexibility_bonus_subsidy(data):
+    policy_row = data["policies"].loc[data["policies"]["policy"] == "Flexibility bonus"]
+    flex_bonus_per_kw_and_year = float(policy_row["value 1"].values[0])
+    flex_bonus_duration = float(policy_row["value 2"].values[0])
+    chp_capacity = receive_chp_capacity(data, "chp to b_electricity")
+
+    general = data["general"]
+    start_time = pd.Timestamp(general.loc[general["label"] == "start_time", "value"].item())
+    end_time = pd.Timestamp(general.loc[general["label"] == "end_time", "value"].item())
+    optimization_horizon_years = ((end_time - start_time).total_seconds() / 3600 + 1) / 8760
+
+    relevant_period_years = min(optimization_horizon_years, flex_bonus_duration)
+    total_bonus = round(flex_bonus_per_kw_and_year * chp_capacity * relevant_period_years, 1)
+
+    return {"flexibility_bonus_remuneration": total_bonus}
+
+
 def receive_pyrolysis_investment(data, scalar_objective):
     investment = (
         data["converters"].loc[data["converters"]["label"] == "pyrolysis", "investment"].values[0]
